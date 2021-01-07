efbd5a284b4214c853c77ee3c00a94cf808dd99b
Daniil-Selikhanovych <selihanovich.do@phystech.edu>
2021-01-07 23:27:31 +0300

    add mala functions

--- tools/ebm_2d_sampling.py
+++ tools/ebm_2d_sampling.py
@@ -74,61 +74,68 @@ def langevin_sampling(gen, dis, alpha, n_steps, step_lr, eps_std, n, batchsize):
     ims = np.asarray(ims).reshape(-1, z.shape[-1])
     zs = np.stack(zs, axis=0)
     return ims, zs
+    
+def mala_dynamics(z, gen, dis, alpha, n_steps, step_lr, eps_std):
+    z_sp = [z.clone().detach()]
+    batch_size, z_dim = z.shape[0], z.shape[1]
+    loc = torch.zeros(z_dim).to(gen.device)
+    scale = torch.ones(z_dim).to(gen.device)
+    
+    normal = Normal(loc, scale)
+    uniform = Uniform(low = 0.0, high = 1.0)
+    acceptence = torch.zeros(batch_size).to(gen.device)
 
-def MALA_sampling(generator, discriminator, 
-                  z_dim, eps, num_iter, device):
-   loc = torch.zeros(z_dim).to(device)
-   scale = torch.ones(z_dim).to(device)
-   normal = Normal(loc, scale)
-   diagn = Independent(normal, 1)
-   uniform_sampler = Uniform(low = 0.0, high = 1.0)
-   cur_z = diagn.sample()
-   cur_z.requires_grad_(True)
-   latent_arr = [cur_z.clone()]
-   for i in range(num_iter):
-      GAN_part = -discriminator(generator(cur_z))
-      latent_part = -diagn.log_prob(cur_z)
-      cur_energy = GAN_part + latent_part 
-      cur_energy.backward()
-      noise = diagn.sample()
-      gamma = eps/2
-      with torch.no_grad():
-         new_z = (cur_z - gamma*cur_z.grad + (eps ** 0.5)*noise)
-      new_z = new_z.clone()
-      new_z.requires_grad_(True)
-      new_energy = -discriminator(generator(new_z)) - diagn.log_prob(new_z)
-      new_energy.backward()
-      energy_part = cur_energy - new_energy
-      with torch.no_grad():
-         vec_for_propose_2 = cur_z - new_z + gamma*new_z.grad
-      propose_part_2 = (vec_for_propose_2 @ vec_for_propose_2)/4.0/gamma
-      propose_part = (noise @ noise)/2.0 - propose_part_2
-      log_accept_prob = propose_part + energy_part
-      generate_uniform_var = uniform_sampler.sample().to(device)
-      log_generate_uniform_var = torch.log(generate_uniform_var)
-      if log_generate_uniform_var < log_accept_prob:
-          latent_arr.append(new_z.clone())
-          cur_z = new_z
-          cur_z.grad.data.zero_()
+    for _ in range(n_steps):
+        noise = normal.sample([batch_size])
+        eps = eps_std * noise
 
-   latent_arr = torch.stack(latent_arr, dim = 0)
-   return latent_arr
+        E, grad = e_grad(z, normal, gen, dis, alpha, ret_e=True)
+        
+        new_z = z - step_lr * grad + eps
+        new_z = new_z.data
+        new_z.requires_grad_(True)
+        
+        E_new, grad_new = e_grad(new_z, normal, gen, dis, alpha, ret_e=True)
+        
+        energy_part = E - E_new
+        
+        propose_vec = z - new_z + step_lr*grad_new
+        
+        propose_part_1 = normal.log_prob(noise)
+        propose_part_2 = normal.log_prob(propose_vec/eps_std)
+        
+        propose_part = torch.sum(propose_part_2 - propose_part_1, dim=1)
 
-def NUTS_sampling(generator, discriminator, z_dim, num_samples, device):
-   cur_calculate_energy = partial(calculate_energy, 
-                                  generator = generator,
-                                  discriminator = discriminator)
-   kernel = NUTS(potential_fn = cur_calculate_energy)
-   loc = torch.zeros(z_dim).to(device)
-   scale = torch.ones(z_dim).to(device)
-   normal = Normal(loc, scale)
-   diagn = Independent(normal, 1)
-   init_params = diagn.sample()
-   init_params = {'points': init_params}
-   mcmc = MCMC(kernel = kernel, 
-               num_samples = num_samples, 
-               initial_params = init_params,
-               num_chains = 1)
-   mcmc.run()
-   latent_arr = mcmc.get_samples()['points']
-   return latent_arr, mcmc
+        log_accept_prob = propose_part + energy_part
+        generate_uniform_var = uniform.sample([batch_size]).to(gen.device)
+        log_generate_uniform_var = torch.log(generate_uniform_var)
+
+        mask = log_generate_uniform_var < log_accept_prob
+        acceptence += mask
+        #print(mask)
+        #print(z)
+        #print(new_z)
+        with torch.no_grad():
+            z[mask] = new_z[mask].detach().clone()
+            z = z.data
+            #print(z)
+            z.requires_grad_(True)
+            z_sp.append(z.clone().detach())
+        
+    return z_sp, acceptence
+    
+def mala_sampling(gen, dis, alpha, n_steps, step_lr, eps_std, n, batchsize):
+    ims = []
+    zs = []
+    for i in tqdm(range(0, n, batchsize)):
+        z = gen.make_hidden(batchsize)
+        z.requires_grad_(True)
+        z_sp, acceptence = mala_dynamics(z, gen, dis, alpha, n_steps, step_lr, eps_std)
+        last_z_sp = z_sp[-1].to(gen.device)
+        x = gen(last_z_sp).data.cpu().numpy()
+        ims.append(x)
+        zs.append(np.stack([o.data.cpu().numpy() for o in z_sp], axis=0))
+
+    ims = np.asarray(ims).reshape(-1, z.shape[-1])
+    zs = np.stack(zs, axis=0)
+    return ims, zs
